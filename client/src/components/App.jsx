d165f672ee8d2980cc51983afc54bdd5674ecbb4
Linh Dang <linh.dang01@gmail.com>
2020-12-28 10:38:04 -0800

    commit to create a branch

---
+++ client/src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class App extends react.components {
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default App
